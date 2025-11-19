20a239651033fb7273a58a4c2930e2df06372957
Tamires Talier <96202019+tamirestalier@users.noreply.github.com>
2025-11-19 07:36:42 -0300

    Atulizando o C#

--- Microfisioterapia.Web/Security/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace Microfisioterapia.Web.Security
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool Verify(string password, string hashedPassword);
-    }
-}
